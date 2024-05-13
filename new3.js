9f10012ba2d035ed43099cd9e0c23ad1531a1f16
IshalImran <163378680+IshalImran@users.noreply.github.com>
2024-05-13 17:57:45 +0500

    Add files via upload

---
+++ new3.js
@@ -0,0 +1,2 @@
+console.log("Ishal imran");
+//node new3console.log ("window");this line will not execute on node js
---
+++ varletconst.js
@@ -0,0 +1,11 @@
+var name = "ishal";
+let age = 10;
+const salary = 100;
+//let age = 01; we cant use it again. cant redeclare, can modify it
+//salary = 10; we cant use it again and we cant modify(reinitialized) bcz 
+//var name = "imran"; we can use it again and also modify
+
+var num1 = 100;
+var num2 = 2.5;
+
+console.log(name + num1 + num2);
